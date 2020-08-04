a3a5b01bc8400ba7c6f12500a8f04ee7d6aa10a7
Kierico Grandao <you@example.com>
2020-08-04 16:10:28 -0400

    KiericoFlix Finalizado!

--- src/pages/cadastro/videos/index.js
+++ src/pages/cadastro/videos/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import useForm from '../../../hooks/useForm';
@@ -10,11 +10,11 @@ import categoriasRepository from '../../../repositories/categorias';
 function CadastraVideo() {
   const history = useHistory();
   const [categorias, setCategorias] = useState([]);
-  const categoryTitles = categorias.map(({titulo}) => titulo);
+  const categoryTitles = categorias.map(({ titulo }) => titulo);
   const { handleChange, values } = useForm({
-    titulo: "Vídeo Novo",
-    url: "https://www.youtube.com/watch?v=SIvxGvSCV_w",
-    categoria: "Front End",
+    titulo: '', //Vídeo Novo
+    url: '', //https://www.youtube.com/watch?v=SIvxGvSCV_w
+    categoria: '', //Front End
   });
 
   useEffect(() => {
@@ -31,13 +31,11 @@ function CadastraVideo() {
 
       <form onSubmit={(event) => {
         event.preventDefault();
-        //alert('Video Cadastrado com sucesso!');
+        // alert('Video Cadastrado com sucesso!');
 
-        const categoriaEscolhida = categorias.find((categoria) => {
-          return categoria.titulo === values.categoria;
-        });
+        const categoriaEscolhida = categorias.find((categoria) => categoria.titulo === values.categoria);
 
-        console.log("categoriaEscolhida", categoriaEscolhida);
+        console.log('categoriaEscolhida', categoriaEscolhida);
 
         videosRepository.create({
           titulo: values.titulo,
@@ -48,7 +46,6 @@ function CadastraVideo() {
             console.log('Cadastro realizado com sucesso!');
             history.push('/');
           });
-
       }}
       >
         <FormField
